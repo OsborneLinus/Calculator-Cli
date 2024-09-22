ee0a4ace8bebfef83da973ca9020672b642ca3d1
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-09-22 19:51:54 -0300

    Logic to Dark Mode

--- app.js
+++ app.js
@@ -101,3 +101,90 @@ function performCalculation() {
   currentNumber = "";
   insertPlace.innerText = result;
 }
+
+//Dark Mode
+let isDark = false
+function darkMode(){
+
+  if(isDark == false){
+
+    document.body.classList.add("bg-zinc-900");
+    document.querySelectorAll(".textToWhite").forEach(e =>
+      e.classList.add("text-white")
+    )
+    document.querySelectorAll(".borderToWhite").forEach(e =>
+      e.classList.add("border-white")
+    )
+
+    //DarkMode button
+    document.querySelector(".darkModeBtn").classList.add("bg-zinc-900")
+    document.querySelector(".darkModeBtn").classList.add("hover:bg-white")
+    document.querySelector(".darkModeBtn").classList.add("hover:text-black")
+
+    //Calculator Buttons
+    document.querySelector(".bg-slate-600").classList.add("bg-neutral-900")
+    document.querySelector(".bg-slate-600").classList.remove("bg-slate-600")
+
+    document.querySelectorAll(".bg-slate-500").forEach(e => {
+      e.classList.add("bg-neutral-800")
+      e.classList.remove("bg-slate-500")
+      e.classList.add("hover:bg-neutral-900")
+      e.classList.remove("hover:bg-slate-600")
+    })
+
+    document.querySelectorAll(".bg-slate-400").forEach(e => {
+      e.classList.add("bg-neutral-700")
+      e.classList.remove("bg-slate-400")
+      e.classList.add("hover:bg-neutral-800")
+      e.classList.remove("hover:bg-slate-500")
+    })
+
+    document.querySelectorAll(".bg-orange-500").forEach(e => {
+      e.classList.add("bg-orange-800")
+      e.classList.remove("bg-orange-500")
+      e.classList.add("hover:bg-orange-900")
+      e.classList.remove("hover:bg-orange-600")
+    })
+    isDark = true
+  }
+  else{
+    document.body.classList.remove("bg-zinc-900");
+    document.querySelectorAll(".textToWhite").forEach(e =>
+      e.classList.remove("text-white")
+    )
+    document.querySelectorAll(".borderToWhite").forEach(e =>
+      e.classList.remove("border-white")
+    )
+
+    //DarkMode button
+    document.querySelector(".darkModeBtn").classList.remove("bg-zinc-900")
+    document.querySelector(".darkModeBtn").classList.remove("hover:bg-white")
+    document.querySelector(".darkModeBtn").classList.remove("hover:text-black")
+
+    //Calculator Buttons
+    document.querySelector(".bg-neutral-900").classList.add("bg-slate-600")
+    document.querySelector(".bg-neutral-900").classList.remove("bg-neutral-900")
+
+    document.querySelectorAll(".bg-neutral-800").forEach(e => {
+      e.classList.remove("bg-neutral-800")
+      e.classList.add("bg-slate-500")
+      e.classList.remove("hover:bg-neutral-900")
+      e.classList.add("hover:bg-slate-600")
+    })
+
+    document.querySelectorAll(".bg-neutral-700").forEach(e => {
+      e.classList.remove("bg-neutral-700")
+      e.classList.add("bg-slate-400")
+      e.classList.remove("hover:bg-neutral-800")
+      e.classList.add("hover:bg-slate-500")
+    })
+
+    document.querySelectorAll(".bg-orange-800").forEach(e => {
+      e.classList.remove("bg-orange-800")
+      e.classList.add("bg-orange-500")
+      e.classList.remove("hover:bg-orange-900")
+      e.classList.add("hover:bg-orange-600")
+    })
+    isDark = false
+  }
+}
